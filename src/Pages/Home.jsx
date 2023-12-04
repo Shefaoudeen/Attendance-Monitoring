6e01fcb7f1a68d96fadd9af0a830de11b77f3cf6
Shefa <shefaoudeen123@gmail.com>
2023-12-04 12:39:45 +0530

    final

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,10 @@ const styling = {
 
 const Home = () => {
   return (
-    <div className=" bg-blue-200 h-screen flex flex-col justify-center items-center relative leading-10">
+    <div
+      className=" bg-blue-200 h-screen flex flex-col justify-center items-center relative leading-10"
+      style={styling}
+    >
       <h1 className="font-extrabold">COMING SOON</h1>
       <div className="border-none border-black p-2.5 rounded-xl mx-2 bg-gray-300 text-white drop-shadow-2xl z-10">
         <div className="text-3xl text-center font-extrabold z-10">
@@ -25,7 +28,7 @@ const Home = () => {
       </div>
       <h1 className="z-10 font-extrabold">WEBSITE UNDER CONSTRUCTION</h1>
       <img src={Attendance} width={250} className="absolute bottom-0 left-0" />
-      <footer className="absolute bottom-0 text-center w-full bg-gray-300 py-2 font-semibold">
+      <footer className="fixed bottom-0 text-center w-full bg-gray-300 py-2 font-semibold">
         &#169; Shefa 2024
       </footer>
     </div>
--- src/Pages/Internal.jsx
+++ src/Pages/Internal.jsx
@@ -6,7 +6,7 @@ const styling = {
 
 const Internal = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div style={styling} className="flex justify-center items-center">
       <h1 className="text-2xl font-bold">PAGE UNDER CONSTUCTION</h1>
       <footer className="absolute bottom-0 text-center w-full bg-gray-300 py-2 font-semibold">
         &#169; Shefa 2024
