55ada03b99016c4a88fdedc28d6329a435b38cdf
Shefa <shefaoudeen123@gmail.com>
2023-12-04 17:55:10 +0530

    added

--- src/Pages/StudentProfile.jsx
+++ src/Pages/StudentProfile.jsx
@@ -18,65 +18,67 @@ const StudentProfile = () => {
           <div className="flex flex-col justify-center w-full items-center my-3">
             <img
               src={student.image}
-              className="w-[300px] h-[300px] rounded-3xl drop-shadow-lg object-fill"
+              className="w-[200px] h-[200px] rounded-3xl drop-shadow-lg object-fill"
             />
           </div>
           <h1 className="font-extrabold text-3xl text-center">
             {student.name}
           </h1>
           <p className="text-xl text-center">{student.regno}</p>
-
-          <table class="table-auto mb-10 mx-2 text-center">
-            <thead>
-              <tr>
-                <th>Subject Name</th>
-                <th>Subject Code</th>
-                <th>Percentage</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Microwave and Optical Engineering</td>
-                <td>EC217</td>
-                <td>%</td>
-              </tr>
-              <tr>
-                <td>Data Communication Networks</td>
-                <td>EC218</td>
-                <td>%</td>
-              </tr>
-              <tr>
-                <td>VLSI Design</td>
-                <td>EC219</td>
-                <td>%</td>
-              </tr>
-              <tr>
-                <td>Control Systems Engineering</td>
-                <td>EC219</td>
-                <td>%</td>
-              </tr>
-              <tr>
-                <td>Industrial Management and Economics</td>
-                <td>HS202</td>
-                <td>%</td>
-              </tr>
-              <tr>
-                <td>Microwave and Optical Engineering Lab</td>
-                <td>EC220</td>
-                <td>%</td>
-              </tr>
-              <tr>
-                <td>Data Communication Networks Lab</td>
-                <td>EC220</td>
-                <td>%</td>
-              </tr>
-              <tr>
-                <td>VLSI Design Lab</td>
-                <td>EC220</td>
-                <td>%</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="border-2 w-fit mx-2 mb-10 rounded-2xl">
+            <table class="table-auto text-center border-collapse drop-shadow-xl">
+              <thead>
+                <tr>
+                  <th>Subject Name</th>
+                  <th class="border-x border-slate-600">Subject Code</th>
+                  <th>Percentage</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Microwave and Optical Engineering</td>
+                  <td class="border-x border-slate-700">EC217</td>
+                  <td>%</td>
+                </tr>
+                <tr>
+                  <td>Data Communication Networks</td>
+                  <td class="border-x border-slate-700">EC218</td>
+                  <td>%</td>
+                </tr>
+                <tr>
+                  <td>VLSI Design</td>
+                  <td class="border-x border-slate-700">EC219</td>
+                  <td>%</td>
+                </tr>
+                <tr>
+                  <td>Control Systems Engineering</td>
+                  <td class="border-x border-slate-700">EC219</td>
+                  <td>%</td>
+                </tr>
+                <tr>
+                  <td>Industrial Management and Economics</td>
+                  <td class="border-x border-slate-700">HS202</td>
+                  <td>%</td>
+                </tr>
+                <tr>
+                  <td>Microwave and Optical Engineering Lab</td>
+                  <td class="border-x border-slate-700">EC220</td>
+                  <td>%</td>
+                </tr>
+                <tr>
+                  <td>Data Communication Networks Lab</td>
+                  <td class="border-x border-slate-700">EC220</td>
+                  <td>%</td>
+                </tr>
+                <tr>
+                  <td>VLSI Design Lab</td>
+                  <td class="border-x border-slate-700">EC220</td>
+                  <td>%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <h1 className="font-extrabold text-center">OVERALL - %</h1>
           <footer className="fixed bottom-0 mt-4 text-center w-full bg-gray-300 py-2 font-semibold">
             &#169; Shefa 2024
           </footer>
